da35125eacaa52cf764686d3f39b9d2fad8cd612
SergeyV1 <sergey.vinickiy@verifone.com>
2020-04-10 15:51:57 +0300

    adding relationship

---
+++ src/main/java/com/vinickiy/Recipe/domain/Ingredient.java
@@ -0,0 +1,59 @@
+package com.vinickiy.Recipe.domain;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+public class Ingredient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+    private BigDecimal amout;
+    @OneToOne
+    private UnitOfMeasure unitOfMeasure;
+
+    @ManyToOne
+    private Recipe recipe;
+
+    public UnitOfMeasure getUnitOfMeasure() {
+        return unitOfMeasure;
+    }
+
+    public void setUnitOfMeasure(UnitOfMeasure unitOfMeasure) {
+        this.unitOfMeasure = unitOfMeasure;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getAmout() {
+        return amout;
+    }
+
+    public void setAmout(BigDecimal amout) {
+        this.amout = amout;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+}
